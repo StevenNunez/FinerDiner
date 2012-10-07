bac4dbbd5267c0ab388336005c5777c0f83e99bc
Steven Nunez <steven.nunez@gmail.com>
2012-10-07 19:03:42 -0400

    Add #total to Order

--- lib/FinerDiner/order.rb
+++ lib/FinerDiner/order.rb
@@ -12,4 +12,15 @@ class Order
   def show
     items.map { |item, count| "#{item.name} x#{count}" }
   end
+
+  def total
+    items.inject(0){|memo, item| memo += total_per_item(item)}
+  end
+
+  private
+
+  def total_per_item(item_array)
+    item, quantity = item_array
+    item.price.to_f * quantity
+  end
 end
