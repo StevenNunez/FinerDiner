bc8241a194e37ba3aaff0c6cd3e8188a54f349c6
Steven Nunez <steven.nunez@gmail.com>
2012-10-07 18:36:21 -0400

    Add #show functionality to return current "cart"

--- spec/FinerDiner/order_spec.rb
+++ spec/FinerDiner/order_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe Order do
-  it "responds to add" do 
+  it "can have items added" do 
     bacon = MenuItem.new
     order = Order.new
     order.add bacon
 
     order.items.should include bacon
+  end
+
+  it "keeps track of item count" do 
+    bacon = MenuItem.new
+    bacon.name = "Bacon"
+
+    eggs = MenuItem.new
+    eggs.name = "Eggs"
+
+    order = Order.new
+
+    order.add bacon
+    order.add eggs
+    order.add eggs
+
+    order.show.should == ["Bacon x1", "Eggs x2"]
   end
 end
