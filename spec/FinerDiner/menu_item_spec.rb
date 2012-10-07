3f76d0fbb5cd77ce8e6af00b11e3ff464430ecdc
Steven Nunez <steven.nunez@gmail.com>
2012-10-07 14:37:18 -0400

    Further clean up

--- spec/FinerDiner/menu_item_spec.rb
+++ spec/FinerDiner/menu_item_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 
 describe MenuItem do
 
+  before do
+    @menu_item = MenuItem.new 
+  end
   it "can have its name set" do 
-    menu_item = MenuItem.new
-    menu_item.name = "Chunky Bacon"
-    menu_item.name.should == "Chunky Bacon"
+    @menu_item.name = "Chunky Bacon"
+    @menu_item.name.should == "Chunky Bacon"
   end
 
   it "can have its price set" do
-    menu_item = MenuItem.new
-    menu_item.price = "2.99"
-    menu_item.price.should == "2.99"
+    @menu_item.price = "2.99"
+    @menu_item.price.should == "2.99"
   end
 
   it "can have its name set" do 
-    menu_item = MenuItem.new
-    menu_item.calorie_count = 256
-    menu_item.calorie_count.should == 256
+    @menu_item.calorie_count = 256
+    @menu_item.calorie_count.should == 256
   end
 
   it "can have its name set" do 
-    menu_item = MenuItem.new
-    menu_item.ingredients = [:bacon, :thats_it]
-    menu_item.ingredients.should == [:bacon, :thats_it]
+    @menu_item.ingredients = [:bacon, :thats_it]
+    @menu_item.ingredients.should == [:bacon, :thats_it]
   end
+
 end
